4b0816eb0971d47219be002da6c18c4502924a4c
Yifan Lu <me@yifanlu.com>
2015-12-26 15:39:26 -0600

    Renamed idp-reg.scm to idp-arch.scm

--- cgen/cgen-idp.scm
+++ cgen/cgen-idp.scm
@@ -21,7 +21,7 @@
   (load (string-append srcdir "/idp-ana.scm"))
   (load (string-append srcdir "/idp-ins.scm"))
   (load (string-append srcdir "/idp-out.scm"))
-  (load (string-append srcdir "/idp-reg.scm"))
+  (load (string-append srcdir "/idp-arch.scm"))
 )
 
 (define opc-arguments
